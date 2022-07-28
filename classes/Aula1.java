ab48ad863d3e3f5de911441c01d3bb6d02b1e4f0
Pavanelli99 <pavanelli1999@gmail.com>
2022-07-27 20:27:41 -0400

    teste

---
+++ classes/Aula1.java
@@ -0,0 +1,25 @@
+package classes;
+
+    public class Aula1 extends Cachorro{
+    
+        public static void main(String[] args){
+
+         Cachorro doguinho = new Cachorro();
+
+         doguinho.nome = "Rex";
+         doguinho.idade = 7;
+         doguinho.raca = "Caramelo";
+         doguinho.dono = true;
+         doguinho.castrado = false;
+         doguinho.peso = 7.3;
+
+         System.out.println("O nome do cahorro é: " + doguinho.nome);
+         
+         Cachorro toto = new Cachorro();
+
+
+
+   
+    
+    }
+}
---
+++ classes/Cachorro.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class Cachorro {
+    //-----------Atributos--------------//
+    private String nome;
+    private int idade;
+    private String raca;
+    private boolean dono;
+    private boolean castrado;
+    private Double peso;
+   
+    //-------- Construtor --------------//
+    public Cachorro(){  // Cria o objeto para a jvm
+       System.out.println("Estou sendo criado com o nome: " + this.nome);
+    }
+    //---------- Metodo ---------------//
+
+
+
+
+}
