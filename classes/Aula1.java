81341a636fc9e2b8bb15c1f829b52bb48d693618
Pavanelli99 <pavanelli1999@gmail.com>
2022-07-27 21:33:22 -0400

    Aula27_07

--- classes/Aula1.java
+++ classes/Aula1.java
@@ -1,23 +1,32 @@
 package classes;
 
-    public class Aula1 extends Cachorro{
+    public class Aula1{
     
         public static void main(String[] args){
 
-         Cachorro doguinho = new Cachorro();
+         Cachorro doguinho = new Cachorro("doguinho");// o name é o 'inlay'
 
-         doguinho.nome = "Rex";
-         doguinho.idade = 7;
-         doguinho.raca = "Caramelo";
-         doguinho.dono = true;
-         doguinho.castrado = false;
-         doguinho.peso = 7.3;
+         doguinho.setNome("Rex");
+         doguinho.setIdade(7);
+         doguinho.setRaca("Caramelo");
+         doguinho.setDono(true);
+         doguinho.setCastrado(false);
+         doguinho.setPeso(7.3);
 
-         System.out.println("O nome do cahorro é: " + doguinho.nome);
+         //System.out.println("O nome do cahorro é: " + doguinho.nome);
+
+         System.out.println(doguinho.toString());
          
-         Cachorro toto = new Cachorro();
+         Cachorro toto = new Cachorro("toto");
 
+         toto.setNome("Birulaybe");
+         toto.setIdade(10);
+         toto.setRaca("Caramelo");
+         toto.setDono(true);
+         toto.setCastrado(false);
+         toto.setPeso(10.0);
 
+         System.out.println(toto.toString());
 
    
     
--- classes/Cachorro.java
+++ classes/Cachorro.java
@@ -10,12 +10,57 @@ public class Cachorro {
     private Double peso;
    
     //-------- Construtor --------------//
-    public Cachorro(){  // Cria o objeto para a jvm
-       System.out.println("Estou sendo criado com o nome: " + this.nome);
+    public Cachorro(String name){  // Cria o objeto para a jvm
+       System.out.println("\nCriando o Objeto: " + name);
     }
-    //---------- Metodo ---------------//
+    //---------- Getters e Setters ---------------//
+     public void setNome (String n){
+        this.nome = n;
+     }
+     public String getNome (){
+        return this.nome;
+     }
+     public void setIdade (int idade){
+        this.idade = idade;
+     }
+     public int getIdade (){
+        return this.idade;
+     }
+     public void setRaca (String raca){
+        this.raca = raca;
+     }
+     public String getRaca (){
+        return this.raca;
+     }
+     public void setDono (boolean dono){
+        this.dono = dono;
+     }
+     public boolean getDono (){
+        return this.dono;
+     }
+     public void setCastrado (boolean castrado){
+        this.castrado = castrado;
+     }
+     public boolean getCastrado (){
+        return this.castrado;
+     }
+     public void setPeso (Double peso){
+        this.peso = peso;
+     }
+     public Double getPeso (){
+        return this.peso;
+     }
 
+     //---------- Metodos ----------------//
+    public String toString(){
 
+        return ("Nome: " + this.nome +
+        "\nIdade: " + this.idade + 
+        "\nRaça: " + this.raca + 
+        "\nDono: " + this.dono + 
+        "\nCastrado: " + this.castrado + 
+        "\nPeso: " + this.peso);
+     }
 
 
 }
